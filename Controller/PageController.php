b850618f12d5aa885dc5c74c6534247e954e1929
Philippe Le Van <philippe.leva@kitpages.fr>
2011-09-16 14:51:30 +0200

    publication time exeeded php timeout

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -620,6 +620,8 @@ class PageController extends Controller
 
     public function publishAction(Page $page)
     {
+        ini_set ('max_execution_time', 3000);
+        ini_set ('memory_limit', "750M");
         $childrenPublish = $this->get('request')->query->get('children', false);
         $this->publish($page, $childrenPublish);
 
