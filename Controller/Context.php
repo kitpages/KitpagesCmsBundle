8eac6d4cb67f6032988d505da4012c0572eadc36
Sebastien Lefebvre <sebastien.lefebvre@kitpages.fr>
2013-11-15 13:55:45 +0100

    fix bug logout context

--- Controller/Context.php
+++ Controller/Context.php
@@ -2,6 +2,8 @@
 namespace Kitpages\CmsBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class Context
 {
@@ -10,9 +12,10 @@ class Context
     const VIEW_MODE_EDIT = 3;  
     private $_session = null;
     
-    public function __construct(Session $session, $viewModeDefault)
+    public function __construct(Session $session, SecurityContext $security, $viewModeDefault)
     {
         $this->_session = $session;
+        $this->security = $security;
         $this->view_mode_default = $viewModeDefault;
     }
 
@@ -25,10 +28,14 @@ class Context
     
     public function getViewMode()
     {
+        $username = null;
         $viewMode = $this->getSession()->get('kitpages_cms_context_view_mode');
-        if (!$viewMode) {
+        $token = $this->security->getToken();
+        if (!$viewMode && $token != null && $this->security->isGranted('ROLE_CMS_ADMIN')) {
             $viewMode = constant("self::$this->view_mode_default");
             $this->getSession()->set('kitpages_cms_context_view_mode', $viewMode);
+        } elseif($token == null || !$this->security->isGranted('ROLE_CMS_ADMIN')) {
+            $viewMode = self::VIEW_MODE_PROD;
         }
         return $viewMode;
     }
